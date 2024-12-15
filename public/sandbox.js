5953a6f865866f95742dd8a1fe9e5544a7bb9030
Lucaatje <luca.thierry.2004@hotmail.com>
2024-12-15 20:33:29 +0100

    exercises 1 - 3
    made exercises 1 - 3

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,3 @@
-"use strict";
 // let greet: Function = () => {
 //   console.log('hello, world');
 // }
@@ -11,9 +10,51 @@ var add = function (a, b, c /*?*/) {
     console.log(a + b);
     console.log(c);
 };
-add(5, 10, 'ninja');
+add(5, 11, 'ninja');
 var minus = function (a, b) {
     return a + b;
 };
 var result = minus(10, 7);
 console.log(result);
+var exampleCondition = function (x) { return x % 2 == 0 ? x + 2 : x - 2; };
+console.log(exampleCondition(5));
+var allNumber = function (n) {
+    var stringSequence = "";
+    var x = 0;
+    while (x <= n) {
+        stringSequence += x;
+        if (x < n) {
+            stringSequence += " ";
+        }
+        x++;
+    }
+    return stringSequence;
+};
+var result1 = allNumber(5);
+console.log(result1);
+var allNumberRev = function (n) {
+    var stringSequence = "";
+    while (n >= 0) {
+        stringSequence += n;
+        if (n >= 0) {
+            stringSequence += " ";
+        }
+        n--;
+    }
+    return stringSequence;
+};
+var result2 = allNumberRev(5);
+console.log(result2);
+var allNumberRange = function (lower) { return function (upper) {
+    var stringSequence = "";
+    while (lower <= upper) {
+        stringSequence += lower;
+        if (lower <= upper) {
+            stringSequence += " ";
+        }
+        lower++;
+    }
+    return stringSequence;
+}; };
+var result3 = allNumberRange(0)(5);
+console.log(result3);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,23 +1,71 @@
-// let greet: Function = () => {
-//   console.log('hello, world');
-// }
-
-// greet = 'hello';
-
-// greet = () => {
-//   console.log('hello, again');
-// }
-
 const add = (a: number, b: number, c/*?*/: number | string = 10): void => {
   console.log(a + b);
   console.log(c);
 }
 
-add(5, 10, 'ninja');
+add(5, 11, 'ninja');
 
 const minus = (a: number, b: number): number => {
   return a + b;
 }
 
 let result = minus(10,7);
-console.log(result);
+console.log(result); 
+
+const exampleCondition = (x : number) => x % 2 == 0 ? x + 2 : x - 2
+
+console.log(exampleCondition(5))
+
+
+const allNumber = (n : number) : string =>
+{
+  let stringSequence = ""
+  let x = 0
+  while (x <= n){
+    stringSequence += x
+    if (x < n){
+      stringSequence += " "
+    }
+    x++
+  }
+  return stringSequence
+}
+
+let result1 = allNumber(5)
+console.log(result1)
+
+
+const allNumberRev = (n : number) : string =>
+{
+  let stringSequence = ""
+  while (n >= 0){
+    stringSequence += n
+    if (n >= 0){
+      stringSequence += " "
+    }
+    n--
+  }
+  return stringSequence
+}
+
+let result2 = allNumberRev(5)
+console.log(result2)
+
+
+const allNumberRange = (lower: number) => (upper: number) : string => {
+  let stringSequence = ""
+  while (lower <= upper) {
+    stringSequence += lower
+    if (lower < upper){
+      stringSequence += " "
+    }
+    lower++
+  }
+  return stringSequence
+}
+
+
+let result3 = allNumberRange(0)(5)
+console.log(result3)
+
+
